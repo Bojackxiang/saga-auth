25cbb40904ddc982242a1074d130abb8f28fe05f
Alex <alex@q-83.com>
2022-12-09 23:07:29 +1100

    commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
-import { Router, Route } from "react-router";
+import { Router, Route, browserHistory } from "react-router";
 import IndexReducer from "./index-reducer";
 import IndexSagas from "./index-sagas";
 import Login from "./login";
 import Signup from "./signup";
 import Widgets from "./widgets";
-import { createBrowserHistory } from "history";
-const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const Wrapper = () => {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(IndexReducer, applyMiddleware(sagaMiddleware));
+  const composeSetup =
+    process.env.NODE_ENV !== "production" &&
+    typeof window === "object" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      : compose;
+  const store = createStore(
+    IndexReducer,
+    composeSetup(applyMiddleware(sagaMiddleware))
+  );
   sagaMiddleware.run(IndexSagas); // 这边就执行了 sagaIndex 这个方法
 
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/widgets" component={Widgets} />
-      </Route>
-    </Router>
-  </Provider>;
+  return (
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/widgets" component={Widgets} />
+        </Route>
+      </Router>
+    </Provider>
+  );
 };
 root.render(<Wrapper />);
--- src/signup/index.jsx
+++ src/signup/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import signupRequest from "./actions";
 
-const SignUp = () => {
+const SignUp = (props) => {
+  console.log({ props });
   return <div>SignUp</div>;
 };
 
-export default SignUp;
+// Grab only the piece of state we need
+const mapStateToProps = (state) => ({
+  signUp: state.signUp,
+});
+
+// Connect our component to redux and attach the `signup` piece
+// of state to our `props` in the component.  Also attach the
+// `signupRequest` action to our `props` as well.
+// 这边就是传入 signupRequest 这个 action creator 和 state
+const connected = connect(mapStateToProps, { signupRequest })(SignUp);
+
+const formed = reduxForm({
+  form: "signUp",
+})(connected);
+
+export default formed;
